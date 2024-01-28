a4d20bf24fbb1ebfb9a52da9055205d2cf174778
GrowlyX <growlyxdev@gmail.com>
2024-01-28 01:51:57 -0500

    chore: prepare gradle multiproject

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,61 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
+    java
     kotlin("jvm") version "1.9.21"
+    id("org.ajoberstar.grgit") version "4.1.1"
 }
 
-group = "com.cloverclient.corp.services"
-version = "1.0-SNAPSHOT"
+var currentBranch: String = grgit.branch.current().name
 
-repositories {
-    mavenCentral()
-    configureBufBuildRepository()
+allprojects {
+    group = "com.cloverclient.corp.services"
+    version = "1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+        bufBuild()
+    }
 }
 
-dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+
+    dependencies {
+        compileOnly(kotlin("stdlib"))
+    }
+
+    kotlin {
+        jvmToolchain(17)
+    }
+
+    tasks {
+        withType<JavaCompile> {
+            options.compilerArgs.add("-parameters")
+            options.fork()
+            options.encoding = "UTF-8"
+        }
+
+        withType<KotlinCompile> {
+            kotlinOptions.javaParameters = true
+            kotlinOptions.jvmTarget = "17"
+        }
+    }
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(17)
-}
-
-fun RepositoryHandler.configureBufBuildRepository()
+fun RepositoryHandler.bufBuild()
 {
     maven {
         name = "cloverBuf"
         url = uri("https://buf.build/gen/maven")
+
         credentials(HttpHeaderCredentials::class)
         authentication {
             create<HttpHeaderAuthentication>("header")
+        }
+
+        content {
+            includeGroup("build.buf.gen")
         }
     }
 }
