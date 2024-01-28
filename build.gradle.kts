387d68b2170c4b95f1db9bbf309ef46c34785460
GrowlyX <growlyxdev@gmail.com>
2024-01-28 01:22:32 -0500

    feat: setup buf repo for buf.build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,12 @@ plugins {
     kotlin("jvm") version "1.9.21"
 }
 
-group = "org.example"
+group = "com.cloverclient.corp.services"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    configureBufBuildRepository()
 }
 
 dependencies {
@@ -18,4 +19,16 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
+}
+
+fun RepositoryHandler.configureBufBuildRepository()
+{
+    maven {
+        name = "cloverBuf"
+        url = uri("https://buf.build/gen/maven")
+        credentials(HttpHeaderCredentials::class)
+        authentication {
+            create<HttpHeaderAuthentication>("header")
+        }
+    }
 }
