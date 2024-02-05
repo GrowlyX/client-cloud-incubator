67401412845c804b3125985762ee580d6207edc0
GrowlyX <growlyxdev@gmail.com>
2024-02-05 00:54:02 -0500

    feat: working authenticated websockets yay

--- gateway/src/test/kotlin/com/cloverclient/corp/gateway/AlbWebSocketTest.kt
+++ gateway/src/test/kotlin/com/cloverclient/corp/gateway/AlbWebSocketTest.kt
@@ -4,13 +4,13 @@ import com.cloverclient.corp.gateway.models.Simple1
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.websocket.*
-import io.ktor.http.*
+import io.ktor.client.request.*
 import io.ktor.serialization.kotlinx.*
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import kotlin.test.Test
 
-class RoutingKtTest
+class AlbWebSocketTest
 {
     @Test
     fun testWebsocketApiV1Start()
@@ -22,12 +22,14 @@ class RoutingKtTest
         }
 
         runBlocking {
-            client.webSocket(
-                method = HttpMethod.Get,
-                host = "127.0.0.1",
-                port = 8080,
-                path = "/start"
-            ) {
+            client.webSocket("wss://gateway.corp.cloverclientdev.com/start", request = {
+                headers {
+                    set(
+                        "Cookie",
+                        "CloverGatewayProdSession-0=${AuthenticatedUnitTests.CookieComponent0}; CloverGatewayProdSession-1=${AuthenticatedUnitTests.CookieComponent1}"
+                    )
+                }
+            }, block = {
                 sendSerialized(
                     Simple1(
                         testMessage = "hors"
@@ -35,7 +37,7 @@ class RoutingKtTest
                 )
 
                 println(receiveDeserialized<Simple1>())
-            }
+            })
         }
     }
 }
